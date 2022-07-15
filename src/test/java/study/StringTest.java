980be7cce7db9d756ae2b8b0066c31b2fbca256e
tasklet1579 <tasklet1579@gmail.com>
2022-07-15 13:09:53 +0900

    test(StringTest) : String 클래스에 대한 학습 테스트 재정리 - split - substring - charAt

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,13 +2,17 @@ package study;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
-    @DisplayName("문자열 분할 테스트")
+    @DisplayName("\"1,2\"을 ,로 split 했을 때 1과 2로 분리되는지 확인한다.")
     @Test
-    void 문자열_분할_테스트() {
+    void splitByComma() {
         // given
         String input = "1,2";
 
@@ -19,9 +23,9 @@ public class StringTest {
         assertThat(actual).contains("1", "2");
     }
 
-    @DisplayName("문자열 분할 정확성 테스트")
+    @DisplayName("\"1\"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지 확인한다.")
     @Test
-    void 문자열_분할_정확성_테스트() {
+    void containsExactly() {
         // given
         String input = "1";
 
@@ -32,51 +36,46 @@ public class StringTest {
         assertThat(actual).containsExactly("1");
     }
 
-    @DisplayName("문자열 추출 테스트")
+    @DisplayName("\"(1,2)\" 값이 주어졌을 때 ()을 제거하고 \"1,2\"를 반환한다.")
     @Test
-    void 문자열_추출_테스트() {
+    void substring() {
         // given
         String input = "(1,2)";
-        String expected = "1,2";
 
         // when
         String actual = input.substring(1, input.length() - 1);
 
         // then
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isEqualTo("1,2");
     }
 
-    @DisplayName("문자열 위치 탐색 테스트")
-    @Test
-    void 문자열_위치_탐색_테스트() {
+    @DisplayName("\"abc\" 값이 주어졌을 때 특정 위치의 문자를 가져오도록 한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"0:a", "1:b", "2:c"}, delimiter = ':')
+    void charAt(int index, char expected) {
         // given
         String input = "abc";
-        char expected = 'a';
 
         // when
-        char actual = input.charAt(0);
+        char actual = input.charAt(index);
 
         // then
         assertThat(actual).isEqualTo(expected);
     }
 
-    @DisplayName("문자열 위치 탐색 예외 테스트")
+    @DisplayName("특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException 이 발생한다.")
     @Test
-    void 문자열_위치_탐색_예외_테스트() {
+    void charAt_throwException_givenIndexGreaterThanLength() {
         // given
         String input = "abc";
 
-        // then
-        assertThatThrownBy(() -> {
-            // when
-            input.charAt(3);
-        }).isInstanceOf(StringIndexOutOfBoundsException.class)
-          .hasMessageContaining("String index out of range: 3");
+        // when & then
+        assertThatThrownBy(() -> input.charAt(3)).isInstanceOf(StringIndexOutOfBoundsException.class)
+                                                 .hasMessageContaining("String index out of range: 3");
 
+        // when & then
         assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
-                .isThrownBy(() -> {
-                    input.charAt(3);
-                })
+                .isThrownBy(() -> input.charAt(3))
                 .withMessageMatching("String index out of range: \\d+");
     }
 }
